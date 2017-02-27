26674520168e8a739437593b92f25fd95926206c
Pradip Patil <pradippatil@users.noreply.github.com>
2017-02-26 20:04:51 -0500

    .

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,7 @@ package=$1
 
 [[ -z  $package ]] && echo "Need package name as input" && exit 1
 GOARM=7 GOARCH=arm GOOS=linux go build -o ./$package/$package ./$package
-scp $package/$package pi@raspberrypi.local:/home/pi/go/bin && echo "Copied $package at /home/pi/go/bin"
+if scp $package/$package pi@raspberrypi.local:/home/pi/go/bin ;then
+    echo "Copied $package at /home/pi/go/bin"
+    [[ -f ./$package/$package ]] && rm ./$package/$package && echo "Removed local copy of ./$package/$package"
+ fi
